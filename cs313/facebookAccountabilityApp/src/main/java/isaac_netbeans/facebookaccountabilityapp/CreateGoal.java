681d8508b441735f7c48c02e4958025669043f4f
Kevin Johnson <kevin.j.johnson257@gmail.com>
2015-07-16 16:38:24 -0600

    Added a barebones CreateGoal servlet. This will read in the data and send it (as a Goal object) to the database class.

---
+++ cs313/facebookAccountabilityApp/src/main/java/isaac_netbeans/facebookaccountabilityapp/CreateGoal.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package isaac_netbeans.facebookaccountabilityapp;
+
+import facebook4j.Facebook;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Kevin Johnson (joh06018 at byui.edu)
+ */
+@WebServlet(name = "CreateGoal", urlPatterns = {"/CreateGoal"})
+public class CreateGoal extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Facebook facebook = (Facebook) request.getSession().getAttribute("facebook");
+        
+        // get the post data and process it
+        // make sure to validate it one more time to ensure all of it is there
+        // and that the goal end time is after the current time.
+        
+        // get the variables
+        
+        
+        // validate said variables
+        
+        
+        // compile the data into a goal object
+        
+        
+        // Send the goal object to the database class
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
