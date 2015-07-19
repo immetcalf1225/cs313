b9cfccd2c0a84f9f1fb8f18886dc043014326cde
swtororphanage <swtorisfun@gmail.com>
2015-07-18 18:51:47 -0600

    add user

---
+++ cs313/facebookAccountabilityApp/src/main/java/isaac_netbeans/facebookaccountabilityapp/User.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package isaac_netbeans.facebookaccountabilityapp;
+
+/**
+ *
+ * @author thefl_000
+ */
+public class User {
+    private String user_id;
+    private String facebook_user;
+    
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getFacebook_user() {
+        return facebook_user;
+    }
+
+    public void setFacebook_user(String facebook_user) {
+        this.facebook_user = facebook_user;
+    }
+    
+}
